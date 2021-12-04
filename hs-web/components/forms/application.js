a3202d66e6b432b07e356bc80f7734c16d59740d
olmul <olmulh@gmail.com>
2021-12-04 15:16:37 -0800

    form w api

--- hs-web/components/forms/application.js
+++ hs-web/components/forms/application.js
@@ -4,12 +4,23 @@ export const ApplicationForm = () => {
     const registerUser = async event => {
         event.preventDefault() // don't redirect the page
         
+        const res = await fetch('/api/apply', {
+            body: JSON.stringify({
+              name: event.target.name.value
+            }),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            method: 'POST'
+          })
+      
+          const result = await res.json() 
     }
 
     return (
         <form onSubmit={registerUser}>
             <label htmlFor="name">Name</label>
-            <input id="name" type="text" autoComplete="name" required />
+            <input id="name" name="name" type="text" autoComplete="name" required />
             <button type="submit">Register</button>
         </form>
     )
--- hs-web/pages/apply/index.js
+++ hs-web/pages/apply/index.js
@@ -1,6 +1,10 @@
+import { ApplicationForm } from '../../components/forms/application'
+
 const Apply = () => {
     return (
-        <div><h1>Apply to hyperscale</h1>
+        <div>
+            <h1>Apply to hyperscale</h1>
+            <ApplicationForm />
         </div>
     )
 };
