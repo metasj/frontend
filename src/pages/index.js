22e02a61bc32772a01bfa42575c3fba1d78648fb
Neel Redkar <neel.redkar@gmail.com>
2021-12-07 11:57:28 -0800

    add noopener

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,6 +98,7 @@ const Landing = () => (
 										key={item.name}
 										href={item.href}
 										target="_blank"
+										rel="noopener noreferrer"
 										className="px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-white hover:bg-opacity-30"
 									>
 										{item.name}
